0cbfa778ca22832c81d895bf01593a66baaadf8e
Bui Thanh Lam <lamthientaihehe@gmail.com>
2020-03-20 18:05:43 +0700

    Refactor source code
    Rename package, variables
    Add more comments
    Create backup folder so that developers can put drafts in there

--- SKYFALL/src/com/company/skyfall/controller/Controller.java
+++ SKYFALL/src/com/company/skyfall/controller/Controller.java
@@ -1,6 +1,7 @@
 
 package com.company.skyfall.controller;
 
+import com.company.skyfall.view.play.PlayLayout;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -17,19 +18,19 @@ import java.util.Optional;
 import java.util.ResourceBundle;
 
 
-public class Controller<VBOX> implements Initializable {
+public class Controller implements Initializable {
 
-    //chuyen sang man hinh Play khi an nut Play
+    //switch to PlayLayout when "Play" button is clicked
     public void play(ActionEvent event) throws Exception{
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        Scene scene = new Scene(battleship.BattleshipMain.createContent(), 1500, 900);
+        Scene scene = new Scene(PlayLayout.createContent(), 1500, 900);
         stage.setTitle("Play");
         stage.setScene(scene);
         stage.setFullScreen(true);
         stage.show();
     }
 
-    //chuyen sang man hinh How to Play khi an nut How To Play
+    //switch to HowToPlayLayout when "How To Play" button is clicked
     public void howToPlay(ActionEvent event) throws  Exception{
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         FXMLLoader loader = new FXMLLoader();
@@ -41,7 +42,7 @@ public class Controller<VBOX> implements Initializable {
         stage.setFullScreen(true);
     }
 
-    //hien ra thong bao khi an nut exit
+    //alert a confirmation when exiting
     public void exit(ActionEvent event) {
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
        ButtonType ExitYesBtn = new ButtonType("Yes");
@@ -49,7 +50,7 @@ public class Controller<VBOX> implements Initializable {
       
        Alert ExitAlert = new Alert(Alert.AlertType.CONFIRMATION);
        ExitAlert.setTitle("Exit Confirmation");
-       ExitAlert.setHeaderText("Do you really want to exit now?");
+       ExitAlert.setHeaderText("Are you sure to exit now?");
        ExitAlert.getButtonTypes().setAll(ExitYesBtn,ExitNoBtn);
 
         Optional<ButtonType> ExitAlertResult=ExitAlert.showAndWait();
@@ -57,9 +58,8 @@ public class Controller<VBOX> implements Initializable {
     }
 
 
-
+    //switch back to MainMenuLayout when "Main Menu" button is clicked
     public static void backToMainMenuFromPlay(ActionEvent event) throws Exception{
-        //chuyen ve Main Menu Scene khi an nut Main Menu
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         FXMLLoader loader = new FXMLLoader();
         loader.setLocation(Controller.class.getResource("../View/MainMenuLayout.fxml"));
@@ -70,7 +70,6 @@ public class Controller<VBOX> implements Initializable {
         stage.setFullScreen(true);
     }
     public void backToMainMenuFromHowToPlay(ActionEvent event) throws Exception{
-        //chuyen ve Main Menu Scene khi an nut Main Menu
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         FXMLLoader loader = new FXMLLoader();
         loader.setLocation(getClass().getResource("../View/MainMenuLayout.fxml"));
@@ -80,10 +79,6 @@ public class Controller<VBOX> implements Initializable {
         stage.setScene(MainMenuScene);
         stage.setFullScreen(true);
     }
-    @FXML
-    private VBox enemyBoard;
-
-
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
