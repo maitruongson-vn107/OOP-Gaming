4801c18bb24a7d07ff2bb337f7781abc17441534
vananh0905 <vananh09052000@gmail.com>
2020-04-21 17:28:16 +0700

    Add files via upload

--- SKYFALL/src/com/company/skyfall/model/AirCraft.java
+++ SKYFALL/src/com/company/skyfall/model/AirCraft.java
@@ -3,34 +3,87 @@ package com.company.skyfall.model;
 import javafx.scene.Parent;
 
 public class AirCraft extends Parent {
-    public int type;
-    public boolean vertical;
-    public int HP;
+    private int type;
+    private boolean vertical;
+    private int HP;
+    private boolean die = false;
+    private Board.Cell head;
 
-    public AirCraft(int model_type, boolean vertical){
-        this.type = model_type;
+    public AirCraft(int type, boolean vertical){
+        this.type = type;
         this.vertical = vertical;
-        this.HP = model_type*100;
+        this.HP = type*100;
     }
 
-    /** Hàm xử lý HP khi BỊ TRÚNG ĐẠN */
+    /** AC being hit by each type of bullet */
 
     //hit by bullet type 1
     public void hitType1(){
-        if(HP > 100) HP -= 100;
+        if (HP > 100) HP -= 100;
         else HP = 0;
     }
     //hit by bullet type 2
     public void hitType2(){
-        if(HP > 50) HP -= 50;
+        if (HP > 50) HP -= 50;
         else HP = 0;
     }
     //hit by bullet type 3
     public void hitType3(){
         HP = 0;
     }
-    //kiem tra dieu kien song
+    //AC was really ALive or not
     public boolean isAlive(){
-        return HP > 0;
+        if (HP > 0 ) return true;
+        if (die == false) {
+            die = true;
+            return false;
+        }
+        return true;
     }
+
+    /** getter and setter */
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public boolean isVertical() {
+        return vertical;
+    }
+
+    public void setVertical(boolean vertical) {
+        this.vertical = vertical;
+    }
+
+    public int getHP() {
+        return HP;
+    }
+
+    public void setHP(int HP) {
+        this.HP = HP;
+    }
+
+    public boolean isDie() {
+        return die;
+    }
+
+    public void setDie(boolean die) {
+        this.die = die;
+    }
+
+    public Board.Cell getHead() {
+        return head;
+    }
+
+    public void setHead(Board.Cell head) {
+        this.head = head;
+    }
+
+    public boolean lostHP() {
+        return HP < type*100;
+    }
+
 }
